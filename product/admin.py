7341acfdc4a4e06616017e82c21c51d2ae9ce1a4
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-07-30 22:47:32 +0600

    is staff added in user serializers

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from product.models import Product, Category, Review
+from product.models import Product, Category, Review, ProductImage
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Review)
+admin.site.register(ProductImage)
